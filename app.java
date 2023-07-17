7b47a157a593851ff45cb3584a3795cbcde525bb
Sunilobi <106995530+Sunilobi@users.noreply.github.com>
2023-07-17 17:21:09 +0530

    Delete app.java

--- app.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Your time is being valued.");
-        System.out.println("You are seeing the demonstration of pipeline built using Jenkins, Git, JFrog Artifactory, Maven, SonarQube, Docker, and Tomcat");
-    }
-}
