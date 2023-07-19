954bbbbd327e5c27248bff77fa8a8851b8ea2c97
Sunilobi <106995530+Sunilobi@users.noreply.github.com>
2023-07-19 18:21:43 +0530

    Update ApplicationTest.java

--- src/test/java/ApploginTest/ApplicationTest.java
+++ src/test/java/ApploginTest/ApplicationTest.java
@@ -4,44 +4,37 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import applogin.login;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ApplicationTest {
-	
-	login login=new login();
-	
-	
-	@Test(priority=1)
-	public void launchapp()
-	{
-		
-		login.setup();
-	}
-	
-	
-	@Test(priority=2)
-	public void VerifyText()
-	{
-		
-		boolean status=login.verify();
-		
-		if(status==true)
-		{
-			Assert.assertTrue(true);
-			System.out.println("Enter text is dispalyed");
-			
-		}else
-		{
-			System.out.println("User need to enter the text");
-			Assert.assertTrue(false);
-			
-			
-		}
-		
-		
-		login.traedown();
-		
-		
-		
+
+	login loginInstance;
+
+	@Test(priority = 1)
+	public void launchapp() {
+		// Set up ChromeOptions for headless mode
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless"); // Add the headless argument
+
+		// Initialize ChromeDriver with headless mode
+		WebDriver driver = new ChromeDriver(options);
+
+		loginInstance.setup();
 	}
 
+	@Test(priority = 2)
+	public void VerifyText() {
+		boolean status = loginInstance.verify();
+
+		if (status) {
+			Assert.assertTrue(true);
+			System.out.println("Enter text is displayed");
+		} else {
+			System.out.println("User needs to enter the text");
+			Assert.assertTrue(false);
+		}
+
+	}
 }
